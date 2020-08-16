7e1d5ff6de063274eedfe921f7d7f124fb63db24
kemora08 <kevin_mora89@hotmail.com>
2020-08-16 15:05:08 -0500

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Todos';
-  todoList: any [] = [];
-}
+  todoList: any[] = [];
+todoTitle: string;
 
-ngOnInit() {
-  this.todoList = [
-    // example of how to make an item in todo list
-    { title: 'Install Angular CLI', isDone: false },
-  
-  ];
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false },
+
+    ]; }
+
+      // adds a todo to our list
+  addTodo(): void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+
+    // resets our todoTitle variable to an empty string
+    this.todoTitle = '';
+  }
 
     // a method to delete an item
-    deleteTodo(todo:any) {
+    deleteTodo(todo: any) {
       const index = this.todoList.findIndex(todoItem => todoItem === todo);
       this.todoList.splice(index, 1);
-      <button type="button" (click)= deleteTodo(todo) class="btn btn-danger btn-sm">Delete</button>
+
     }
-}
+  }
+
